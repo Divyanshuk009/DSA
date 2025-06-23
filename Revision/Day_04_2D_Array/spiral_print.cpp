e947d0b2e49b2d10f0a81899a4af9a5890c702dc
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-06-23 20:46:04 +0530

    Revision Day_04_2D_Array : spiral_print

---
+++ Revision/Day_04_2D_Array/spiral_print.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+
+    vector<int> ans;
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int count = 0;
+    int total = row * col;
+
+    int startingRow = 0;
+    int startingCol = 0;
+    int endingRow = row - 1;
+    int endingCol = col - 1;
+
+    while (count < total)
+    // print startinng row
+    {
+        for (int index = startingRow; index <= endingCol; index++)
+        {
+            ans.push_back(matrix[startingRow][index]);
+            count++;
+        }
+        startingRow++;
+
+        // print ending column
+
+        for (int index = startingRow; index <= endingRow; index++)
+        {
+            ans.push_back(matrix[index][endingCol]);
+            count++;
+        }
+        endingCol--;
+
+        //  print ending row
+        for (int index = endingCol; index >= startingCol; index--)
+        {
+            ans.push_back(matrix[endingRow][index]);
+            count++;
+        }
+        endingRow--;
+
+        // print starting column
+
+        for (int index = endingRow; index >= startingRow; index--)
+        {
+            ans.push_back(matrix[index][startingCol]);
+            count++;
+        }
+
+        startingCol++;
+    }
+    return ans;
+}
