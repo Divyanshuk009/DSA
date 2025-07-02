b8fab8202868a8b2793f19045052d9ab94a4931b
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-07-02 11:32:20 +0530

    Recursion : linearSearch

---
+++ recursion/linearSearch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int arr[], int size, int k)
+{
+
+    // base case
+    if (size == 0)
+    {
+        return false;
+        /* code */
+    }
+    if (arr[0] == k)
+    {
+        return true;
+        /* code */
+    }
+    else
+    {
+        bool remainingPart = linearSearch(arr + 1, size - 1, k);
+        return remainingPart;
+    }
+}
+
+int main()
+{
+    int arr[5] = {3, 5, 1, 2, 6};
+    int size = 5;
+    int key = 2;
+    bool ans = linearSearch(arr, size, key);
+    if (ans)
+    {
+        cout << "present";
+    }
+    else
+    {
+        cout << "absent";
+    }
+}
