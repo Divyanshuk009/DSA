3c748d894421f0deb4263928d8743f24f9de04a9
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-07-08 00:14:24 +0530

    Strings & Char Array : reverse_string

---
+++ Char_Array_String/reverse_string.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+void reverse(char name[], int n)
+{
+    int s = 0;
+    int e = n - 1;
+
+    while (s <= e)
+    {
+        swap(name[s++], name[e--]);
+    }
+}
+
+int main()
+{
+
+    char naam[] = "12345";
+
+    reverse(naam, 5);
+
+    cout << naam;
+}
+
+// or
+
+// void reverse(string &st, int si)
+// {
+//     int s = 0;
+//     int e = st.size() - 1;
+
+//     int mid = s + (e - s) / 2;
+//     while (s < e)
+//     {
+//         swap(st[s++], st[e--]);
+//     }
+// }
+
+// int main()
+// {
+//     string name = "golu";
+//     reverse(name, 4);
+//     cout << name;
+// }
