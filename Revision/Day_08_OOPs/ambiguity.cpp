1c1e08e482ef86170a30c7a274ed94474cf19c81
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-07-20 11:25:15 +0530

    Revision Day_08_OOPs : ambiguity

---
+++ Revision/Day_08_OOPs/ambiguity.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    void func()
+    {
+        cout << "i am A " << endl;
+    }
+};
+
+class B
+{
+public:
+    void func()
+    {
+        cout << "i am B " << endl;
+    }
+};
+
+class C : public A, public B
+{
+};
+
+int main()
+
+{
+    C obj;
+    // obj.func(); // func() is ambigus
+    obj.A::func();
+    obj.B::func();
+}
