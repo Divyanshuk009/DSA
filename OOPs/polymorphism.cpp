1953a49bde608d313acdaddcdbe7c215f2e93451
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-07-20 11:32:19 +0530

    OOPs : polymorphism

---
+++ OOPs/polymorphism.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    void sayHello()
+    {
+        cout << "hello love babbar " << endl;
+    }
+    void sayHello(string name)
+    {
+        cout << "hello " << name << endl;
+    }
+};
+
+int main()
+{
+
+    A obj;
+    obj.sayHello();
+    obj.sayHello("golu");
+}
