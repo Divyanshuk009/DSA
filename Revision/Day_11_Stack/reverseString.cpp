9fc385910ca6de0d11e5b36850bc37e218336844
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-07-26 11:15:58 +0530

    Revision Day_11_Stack : reverseString

---
+++ Revision/Day_11_Stack/reverseString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    string str = "divyanshu";
+
+    stack<char> s;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char ch = str[i];
+        s.push(ch); // take ch from str then push to s
+    }
+
+    string ans = "";
+    while (!s.empty())
+    {
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+
+    cout << "answer is: " << ans << endl;
+
+    return 0;
+}
