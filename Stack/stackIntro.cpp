0e58c8f5a17bf3e29a7c4ee67742a31ebf3249b2
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-07-28 21:31:27 +0530

    Stack : stackIntro

---
+++ Stack/stackIntro.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Stack
+{
+
+    // properties
+public:
+    int *arr;
+    int top;
+    int size;
+
+    // behaviours
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
+            top++;
+            arr[top] = element;
+        }
+        else
+        {
+            cout << "stack overflow " << endl;
+        }
+    }
+
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+        }
+        else
+        {
+            cout << "stack underflow " << endl;
+        }
+    }
+
+    int peek()
+    {
+        if (top >= 0)
+        {
+            return arr[top];
+        }
+        else
+        {
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+
+    Stack st(5);
+
+    st.push(23);
+    st.push(45);
+    st.push(65);
+    st.push(12);
+    st.push(75);
+
+    st.push(100); // stack overflow
+
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
+    st.pop();
+    cout << st.peek() << endl;
+
+    // stack<int> s;
+    // s.push(2);
+    // s.push(3);
+    // s.push(5); // last in
+
+    // // s.pop();
+
+    // cout << "top element " << s.top() << endl;
+    // cout << "is empty " << s.empty() << endl;
+
+    return 0;
+}
