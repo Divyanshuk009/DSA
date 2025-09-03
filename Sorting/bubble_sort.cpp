3a5ef9f713755218ea360646351e36488f6f784c
Divyanshu Kumar <divyanshuk899@gmail.com>
2025-09-03 09:12:20 +0530

    Sorting : bubble_sort

---
+++ Sorting/bubble_sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void bbSort(int arr[], int si)
+{
+
+    for (int i = 1; i < si; i++) // to traverse all element in the array
+    {
+
+        // bool swapped = false;
+        for (int j = 0; j < si; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                // swapped = true;
+            }
+        }
+        // if (swapped == false)
+        // {
+        //     break;
+        // }
+    }
+}
+
+int main()
+{
+
+    int num[6] = {8, 3, 4, 7, 1, 5};
+    bbSort(num, 6);
+
+    for (int i = 0; i < 6; i++)
+    {
+
+        cout << num[i] << " ";
+
+        /* code */
+    }
+
+    return 0;
+}
