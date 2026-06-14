0c87ae1d5aef2f5084adcc7fcfefd9a35e3615aa
Divyanshu Kumar <divyanshuk899@gmail.com>
2026-06-14 21:35:34 +0530

    Binary Tree : boundaryTraversal

---
+++ Binary Tree/boundaryTraversal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class node
+{
+
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+    node *buildtree(node*root){
+        cout << "enter data " << endl;
+        if ()
+        {
+            /* code */
+        }
+        
+    }
+};
