63a2756a9e385a0baa28144498b6aef1f2dc033f
Divyanshu Kumar <divyanshuk899@gmail.com>
2026-06-28 10:36:53 +0530

    Revision Day_03_Char_Array_String : rough

---
+++ Revision/Day_03_Char_Array_String/rough.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm> // For std::max
+
+using namespace std;
+#include <vector>
+#include <queue>
+#define g 9.8
+
+// int freq(int num[], int size)
+// {
+
+//     int count = 0;
+//     for (int i = 0; i < size - 1; i++)
+//     {
+//         for (int j = size - 1; j > 0; j--)
+//         {
+//             if (num[i] == num[j])
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// int main()
+// {
+//     int num[] = {3, 4, 1, 2, 1, 1, 5, 1};
+//     cout << freq(num, 8);
+// }
+std::cout << "Enter name: ";
