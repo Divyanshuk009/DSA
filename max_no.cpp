0ebfe14fd8dedb50d3fc7da844e7fbd9e3aae76f
Divyanshu Kumar <divyanshuk899@gmail.com>
2026-08-15 09:32:45 +0530

    Add max_no

---
+++ max_no.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+class Solution
+{
+public:
+    int maximumCount(vector<int> &nums)
+    {
+        int neg = 0;
+        int pos = 0;
+ 
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < 0)
+            {
+                neg += 1;
+            }
+            else if (nums[i] > 0)
+            {
+                pos += 1;
+            }
+        }
+        return max(neg, pos);
+    }
+};
